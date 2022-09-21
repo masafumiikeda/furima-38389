a95bc22cb5597695a212daaae18085b63c2595d7
masafumiikeda <244marshall144@gmail.com>
2022-09-21 16:24:50 +0900

    バリデーション及びストロングパラメーターの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   
   validates :nickname, presence: true
   validates :last_name, presence: true
-  validates :frist_name, presence: true
+  validates :first_name, presence: true
   validates :last_name_kana, presence: true
-  validates :frist_name_kana, presence: true
+  validates :first_name_kana, presence: true
   validates :birthday, presence: true
 end
