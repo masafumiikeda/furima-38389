d7c0dabfb3caf7f4bd6c897238f7638cabb99aa1
masafumiikeda <244marshall144@gmail.com>
2022-09-26 12:07:19 +0900

    アクティブハッシュのモデルクラスの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
-
   belongs_to :user
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery
+  belongs_to :prefecture
+  belongs_to :duration
+
+  validates :name, :description, presence: true
+  validates :price, presence:true, numericality: { in: 300..9999999 }
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :duration_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
