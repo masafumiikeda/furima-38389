b7655f70c63d41ee3f933c4a195371bdfaf7b408
masafumiikeda <244marshall144@gmail.com>
2022-09-27 11:23:16 +0900

    レビュー内容を反映
    ・テストコードに
    「正常系テストコード」
    「商品画像が空では出品出来ない」
    「価格に半角数字以外が含まれている場合は出品できない」
    「userが紐付いていなければ出品できない」
    を追加
    
    ・案内コメントの削除
    ・FactoryBotにuserモデルとのアソシエーションを追記
    ・indexアクション内のインスタンス変数を定義している部分をコメントアウト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,56 +6,76 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品出品機能' do
-    context '商品が出品できないとき' do
-      it 'nameが空では登録できない' do
+    context '商品が出品できる場合' do
+      it "name,description,category_id,status_id,delivery_id,prefecture_id,duration_id,priceが存在すれば出品できる" do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品が出品できない場合' do
+      it 'nameが空では出品できない' do
         @item.name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
-      it 'descriptionが空では登録できない' do
+      it 'descriptionが空では出品できない' do
         @item.description = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
-      it 'category_idが1では登録できない' do
+      it 'category_idが1では出品できない' do
         @item.category_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it 'status_idが1では登録できない' do
+      it 'status_idが1では出品できない' do
         @item.status_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
       end
-      it 'delivery_idが1では登録できない' do
+      it 'delivery_idが1では出品できない' do
         @item.delivery_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery can't be blank")
       end
-      it 'prefecture_idが1では登録できない' do
+      it 'prefecture_idが1では出品できない' do
         @item.prefecture_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
-      it 'duration_idが1では登録できない' do
+      it 'duration_idが1では出品できない' do
         @item.duration_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Duration can't be blank")
       end
-      it 'priceが空では登録できない' do
+      it 'priceが空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it 'priceは300未満では登録できない' do
+      it 'priceは300未満では出品できない' do
         @item.price = '100'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
-      it 'priceは9999999以上では登録できない' do
+      it 'priceは9999999以上では出品できない' do
         @item.price = '99999999'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it '商品画像が空では出品できない' do
+        @item.image = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include()
+      end
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = '9,999'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'userが紐付いていなければ出品できない' do
+        @item.user_id = ''
+        @item.valid?1
+        expect(@item.errors.full_messages).to include()
       end
     end
   end
