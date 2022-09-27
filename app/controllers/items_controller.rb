6506a62a89103cdaae303d316a845d2b04d6b1b9
masafumiikeda <244marshall144@gmail.com>
2022-09-27 20:33:32 +0900

    before_actionの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :set_tweet, only: [:edit, :show] 
+  before_action :set_item, only: [:show] 
 
   def index
     @items = Item.order("created_at DESC")
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_id, :prefecture_id, :duration_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def set_tweet
+  def set_item
     @item = Item.find(params[:id])
   end
 end
