715c5ad6aaa4f508ab16305db483c47b566f3f2f
masafumiikeda <244marshall144@gmail.com>
2022-10-04 19:27:25 +0900

    売却済みの商品購入ページのトップページへのリダイレクト機能追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,14 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :item_set
+  before_action :item_soldout
+  before_action :item_empty
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -31,5 +32,17 @@ class OrdersController < ApplicationController
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
+  end
+
+  def item_set
+    @item = Item.find(params[:item_id])
+  end
+
+  def item_soldout
+    redirect_to root_path if @item.user.id == current_user.id
+  end
+
+  def item_empty
+      redirect_to root_path if @item.order
   end
 end
